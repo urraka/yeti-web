ced17b6af4b729efca75eca2d5ac9f198118452b
urraka <perroazul64@gmail.com>
2015-04-10 10:06:28 -0300

    separated prototype from main project

---
+++ editor-files/objects01.js
@@ -0,0 +1,74 @@
+yeti_objects([
+	{
+		"type": "tree",
+		"group": "G1",
+		"caption": "tree",
+		"src": "tree.png",
+		"cx": 16,
+		"cy": 63,
+		"bounds": {"w": 11, "h": 60}
+	},
+	{
+		"type": "rock",
+		"group": "G1",
+		"caption": "rock",
+		"src": "rock.png",
+		"cx": 11,
+		"cy": 11,
+		"bounds": {"w": 23, "h": 11}
+	},
+	{
+		"type": "skiman",
+		"group": "G1",
+		"caption": "ski",
+		"src": "skiman.png",
+		"cx": 11,
+		"cy": 24,
+		"bounds": {"w": 18, "h": 25}
+	},
+	{
+		"type": "axe",
+		"group": "G1",
+		"caption": "axe",
+		"src": "axe.png",
+		"cx": 11,
+		"cy": 22,
+		"bounds": {"w": 21, "h": 0}
+	},
+	{
+		"type": "flag-left",
+		"group": "G1",
+		"caption": "flg>",
+		"src": "flag-left.png",
+		"cx": 6,
+		"cy": 24,
+		"bounds": {"w": 12, "h": 24}
+	},
+	{
+		"type": "flag-right",
+		"group": "G1",
+		"caption": "flg<",
+		"src": "flag-right.png",
+		"cx": 6,
+		"cy": 24,
+		"bounds": {"w": 12, "h": 24}
+	},
+	{
+		"type": "path",
+		"group": "G1",
+		"caption": "path",
+		"src": "path-dot-1.png",
+		"cx": 4,
+		"cy": 4,
+		"bounds": {"w": 8, "h": 0}
+	}
+	{
+		"type": "berryred",
+		"group": "G1",
+		"caption": "berryred",
+		"src": "berryred.png",
+		"cx": 5,
+		"cy": 16,
+		"bounds": {"w": 32, "h": 17}
+	},
+]);
